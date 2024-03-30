01fb3b7d6206c44c799318d1e4aaf7ac1f50f8f2
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-03-30 12:33:58 +0200

    create data.js and assets forlder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Accordian from "./components/Accordian";
 
 function App() {
   return (
     <div className="App">
-      
+      <Accordian/>
     </div>
   );
 }
---
+++ src/components/Accordian.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import data from './data';
+
+const Accordian = () => {
+  return (
+    <div>Accordian</div>
+  )
+}
+
+export default Accordian
